83ff3533106ac9d068183516eb45e5ee62c07ac8
Aristid Breitkreuz <aristidb@gmail.com>
2014-08-13 21:16:27 +0200

    well that seems to wokr kind of

--- Newmodel.hs
+++ Newmodel.hs
@@ -6,8 +6,11 @@ import qualified Data.Map as M
 
 -- TODO: do not rely on list ordering so much?
 
+{-
 data Scalar = SString String
     deriving (Eq, Ord, Show)
+-}
+type Scalar = String
 
 newtype Value = Value [Fact]
     deriving (Show, Monoid)
@@ -37,8 +40,7 @@ data Fact2
 data Value3
     = Nil
     | ScalarV Scalar
-    | MapC String (M.Map Value3 Value3) -- Nil key for "this context not used here"
-    | SeqC String Value3 [Value3] -- first Value3 is nil-Value
+    | SeqC String Value3 [(Value3, Value3)] -- first Value3 is nil-Value
     deriving (Eq, Show)
 -- ascending keys with depth
 
@@ -48,6 +50,20 @@ instance Monoid Value3 where
     mempty = Nil
     mappend Nil b = b
     mappend a Nil = a
-    mappend (MapC s1 a) (MapC s2 b) | s1 == s2 = MapC s1 (M.union a b)
-                                    | s1 < s2 = MapC s1 (M.insert Nil (MapC s2 b) a)
-                                    | s1 > s2 = MapC s2 (M.insert Nil (MapC s1 a) b)
+    mappend (ScalarV _) y@(ScalarV _) = y
+    mappend x@(ScalarV _) (SeqC s y b) = SeqC s (x `mappend` y) b
+    mappend (SeqC s x a) y@(ScalarV _) = SeqC s (x `mappend` y) a
+    mappend (SeqC s1 x a) (SeqC s2 y b) | s1 == s2 = SeqC s1 (x `mappend` y) (a ++ b)
+                                        | s1 < s2 = SeqC s1 (x `mappend` SeqC s2 y b) a
+                                        | otherwise = SeqC s2 (SeqC s1 x a `mappend` y) b
+
+embed :: String -> Value3 -> Value3 -> Value3
+embed s k Nil = SeqC s Nil [(k, Nil)]
+embed s k v@(ScalarV _) = SeqC s Nil [(k, v)]
+embed s1 k v@(SeqC s2 x a) | s1 == s2 = SeqC s1 x (map (\(k0, v0) -> (k0 `mappend` k, v0)) a)
+                           | s1 < s2 = SeqC s1 Nil [(k, v)]
+                           | otherwise = SeqC s2 x (map (\(k0, v0) -> (k0, embed s1 k v0)) a)
+
+newtype V = V [StreamItem]
+
+data StreamItem = EnterContext String V | LeaveContext String V | Data V
